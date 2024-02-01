a11df3c6971147ba46efbb2ea041c9cc7415684b
weccode <guillermo@weccode.com>
2024-02-01 18:39:17 -0500

    modified package.json to test type and test

--- PasswordChecker.test.js
+++ PasswordChecker.test.js
@@ -1,19 +1,11 @@
-import { checkPasswordWithoutRegEx } from './PasswordChecker';
+import { checkPassword, checkPasswordWithoutRegEx } from './PasswordChecker.js';
+import {describe, test} from 'node:test';
+import * as assert from 'node:assert';
 
-// Test case 1: Password is valid
-console.log(checkPasswordWithoutRegEx('Abc123!@')); // Expected output: "Password is valid"
+describe('checkPassword', () => {
+    const sut = checkPassword;
 
-// Test case 2: Password is too short
-console.log(checkPasswordWithoutRegEx('Abc123')); // Expected output: "Password must be at least 8 characters long"
-
-// Test case 3: Password does not contain an uppercase letter
-console.log(checkPasswordWithoutRegEx('abc123!@')); // Expected output: "Password must contain at least one uppercase letter"
-
-// Test case 4: Password does not contain a lowercase letter
-console.log(checkPasswordWithoutRegEx('ABC123!@')); // Expected output: "Password must contain at least one lowercase letter"
-
-// Test case 5: Password does not contain a number
-console.log(checkPasswordWithoutRegEx('Abcdefg!@')); // Expected output: "Password must contain at least one number"
-
-// Test case 6: Password does not contain a special character
-console.log(checkPasswordWithoutRegEx('Abc123456')); // Expected output: "Password must contain at least one special character"
+    test('throws an error if the password is less than 8 characters long', () => {
+        assert.throws(() => sut(123), Error);
+    });
+});
